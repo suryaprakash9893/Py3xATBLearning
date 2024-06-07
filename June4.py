6c235f47198127bf15be4408a4495bbfb9a27b6b
suryaprakash9893 <suryaprakasha@live.in>
2024-06-06 20:52:44 -0400

    second commit

--- June4.py
+++ June4.py
@@ -1,2 +1,11 @@
 print("Hello World!","i am coding python", end="@", sep="//")
-print(max(10,5))
+print(max(10 , 5))
+age = 5
+print(type(age))
+
+name1='Surya'
+name2="Surya"
+name3="""Surya"""
+print(type(name1))
+print(type(name2))
+print(type(name3))
